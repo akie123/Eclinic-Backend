4f7aac45ac2288de29a51b05cfac0be1f3ab4725
akie123 <aalhadakate@gmail.com>
2023-03-25 20:44:24 +0530

    upd

--- server.js
+++ server.js
@@ -26,6 +26,7 @@ app.use(express.json(),(err,req,res,next) => {      // bodyparse middle ware che
 app.use(cors())      // cors middleware
 
 // Connection to Database
+okay
 mongoose.set("strictQuery", true);
 mongoose.connect(MONGO_URI, {
   useNewUrlParser: true,
