b3163cd68e38fb4607d490639ab3d0cc5b60792b
bargavkoduri <bhargavkoduri123@gmail.com>
2023-03-25 22:02:27 +0530

    changed jwt

--- controllers/doctorS.js
+++ controllers/doctorS.js
@@ -1,15 +1,19 @@
 const Doc = require("../models/doctor")
-const {hashPassword} = require("../utils/bcrypt")
+const { hashPassword } = require("../utils/bcrypt")
 
-const DRController = async(req,res) => {
+const DRController = async (req, res) => {
     req.body['verified'] = false
     req.body.password = hashPassword(req.body.password)
     const new_user = new Doc(req.body)
-    new_user.save().then(()=>{
+    new_user.save()
+    .then(() => {
         res.status(201).json({
-            status:"ok"
-        })
+            status: "ok",
+        });
     })
+    .catch(() => {
+        res.sendStatus(400);
+    });
 }
 
 module.exports = DRController
